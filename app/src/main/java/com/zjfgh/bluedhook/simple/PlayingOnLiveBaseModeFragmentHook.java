027a7f531891559ab4daa0e9a8576a58ef2c9bef
唐纳德 <258436911@qq.com>
2025-06-19 17:07:46 +0800

    Update PlayingOnLiveBaseModeFragmentHook.java
    空壳

--- app/src/main/java/com/zjfgh/bluedhook/simple/PlayingOnLiveBaseModeFragmentHook.java
+++ app/src/main/java/com/zjfgh/bluedhook/simple/PlayingOnLiveBaseModeFragmentHook.java
@@ -1,6 +1,19 @@
-public class User {
-    public String name;
-    public String uid;
-    public String live;
-    public String avatar;
+package com.zjfgh.bluedhook.simple;
+
+import android.content.Context;
+
+public class PlayingOnLiveBaseModeFragmentHook {
+    private static PlayingOnLiveBaseModeFragmentHook instance;
+    private final User watchingAnchor = new User();
+
+    public static synchronized PlayingOnLiveBaseModeFragmentHook getInstance(Context appContext, Object moduleRes) {
+        if (instance == null) {
+            instance = new PlayingOnLiveBaseModeFragmentHook();
+        }
+        return instance;
+    }
+
+    public User getWatchingAnchor() {
+        return watchingAnchor;
+    }
 }
