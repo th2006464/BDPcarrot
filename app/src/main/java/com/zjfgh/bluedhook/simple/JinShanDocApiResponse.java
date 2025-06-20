0e93b77b120b37cdc888d499d9db92a4b683adc0
唐纳德 <258436911@qq.com>
2025-06-20 14:33:56 +0800

    空壳 JinShanDocApiResponse.java

--- app/src/main/java/com/zjfgh/bluedhook/simple/JinShanDocApiResponse.java
+++ app/src/main/java/com/zjfgh/bluedhook/simple/JinShanDocApiResponse.java
@@ -1,4 +1,37 @@
 package com.zjfgh.bluedhook.simple;
 
 public class JinShanDocApiResponse {
+    private Data data;
+
+    public Data getData() {
+        return data != null ? data : new Data();
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public static class Data {
+        private String result;
+
+        public String getResult() {
+            return result != null ? result : "";
+        }
+
+        public void setResult(String result) {
+            this.result = result;
+        }
+    }
+
+    public static class ResultData {
+        private int code;
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+    }
 }
