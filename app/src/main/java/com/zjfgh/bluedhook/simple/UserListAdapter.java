93682730fac38fe11fc7dc3f120fe1436efbc211
唐纳德 <258436911@qq.com>
2025-07-15 16:27:16 +0800

    虚拟框架兼容

--- app/src/main/java/com/zjfgh/bluedhook/simple/UserListAdapter.java
+++ app/src/main/java/com/zjfgh/bluedhook/simple/UserListAdapter.java
@@ -1,189 +1,271 @@
 package com.zjfgh.bluedhook.simple;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.ImageView;
+import android.view.Window;
+import android.view.WindowInsetsController;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.DiffUtil;
-import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONArray;
-import com.alibaba.fastjson2.JSONObject;
 
-import java.lang.reflect.InvocationTargetException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class UserListAdapter extends ListAdapter<User, UserListAdapter.UserViewHolder> {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class UserPopupWindow {
+    private PopupWindow popupWindow;
+    private RecyclerView recyclerView;
+    private UserListAdapter adapter;
+    private Button anchorStartButton;
+
+    @SuppressLint("StaticFieldLeak")
+    private static UserPopupWindow instance;
+
+    private Handler handler;
+    private Runnable checkRunnable;
+    private int currentCheckIndex = 0;
+    private boolean isChecking = false;
+
+    // 保存原始状态
+    private int originalStatusBarColor;
+    private int originalNavigationBarColor;
+    private int originalSystemUiVisibility;
+    private boolean originalLightStatusBars;
+    private boolean originalLightNavBars;
+    private boolean hasSavedOriginalState = false;
+
     private final SQLiteManagement dbManager = SQLiteManagement.getInstance();
-    private final Context context;
-    private String currentCheckingUid = null;
 
-    public interface OnUserDeleteListener {
-        void onUserDelete(User user);
-    }
-
-    private final OnUserDeleteListener deleteListener;
-
-    protected UserListAdapter(@NonNull Context context, OnUserDeleteListener deleteListener) {
-        super(new UserDiffCallback());
-        this.context = context;
-        this.deleteListener = deleteListener;
-    }
-
-    @NonNull
-    @Override
-    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        int layoutId = getResId(context, "anchor_monitor_item_layout", "layout");
-        View view = LayoutInflater.from(context).inflate(
-                AppContainer.getInstance().getModuleRes().getLayout(layoutId), parent, false);
-        return new UserViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
-        User user = getItem(position);
-        try {
-            holder.bind(user);
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-            throw new RuntimeException(e);
+    public static UserPopupWindow getInstance() {
+        if (instance == null) {
+            instance = new UserPopupWindow();
         }
+        return instance;
     }
 
-    public void setCurrentCheckingUid(String uid) {
-        this.currentCheckingUid = uid;
-        notifyDataSetChanged();
+    public void show(Context context) {
+        if (popupWindow != null && popupWindow.isShowing()) return;
+
+        Activity activity = (Activity) context;
+
+        if (!hasSavedOriginalState) {
+            saveOriginalSystemBarState(activity);
+            hasSavedOriginalState = true;
+        }
+
+        int layoutId = getResId(context, "anchor_monitor_layout", "layout");
+        View rootView = LayoutInflater.from(context)
+                .inflate(AppContainer.getInstance().getModuleRes().getLayout(layoutId), null);
+
+        int listId = getResId(context, "anchor_monitor_list", "id");
+        LinearLayout anchorMonitorList = rootView.findViewById(listId);
+
+        int buttonId = getResId(context, "anchor_start_button", "id");
+        anchorStartButton = rootView.findViewById(buttonId);
+
+        recyclerView = new RecyclerView(context);
+        LinearLayout.LayoutParams recyclerParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        recyclerView.setLayoutParams(recyclerParams);
+        anchorMonitorList.addView(recyclerView);
+
+        adapter = new UserListAdapter(context, user -> delAnchor(user.getUid(), user.getName()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(adapter);
+
+        DividerItemDecoration divider = new DividerItemDecoration(context, DividerItemDecoration.VERTICAL);
+        GradientDrawable transparentDivider = new GradientDrawable();
+        transparentDivider.setSize(0, 10);
+        transparentDivider.setColor(Color.TRANSPARENT);
+        divider.setDrawable(transparentDivider);
+        recyclerView.addItemDecoration(divider);
+
+        int neonButtonRes = getResId(context, "neon_button", "drawable");
+        Drawable buttonBg = AppContainer.getInstance().getModuleRes().getDrawable(neonButtonRes, null);
+        anchorStartButton.setBackground(buttonBg);
+        anchorStartButton.setText(isChecking ? "停止检测" : "开始定时检测");
+
+        anchorStartButton.setOnClickListener(v -> {
+            if (isChecking) {
+                stopChecking();
+                anchorStartButton.setText("开始定时检测");
+            } else {
+                startChecking();
+                anchorStartButton.setText("停止检测");
+            }
+        });
+
+        popupWindow = new PopupWindow(
+                rootView,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                true
+        );
+        popupWindow.setOnDismissListener(() -> restoreOriginalSystemBarState(activity));
+        popupWindow.setAnimationStyle(android.R.style.Animation_Dialog);
+        popupWindow.showAtLocation(rootView, Gravity.CENTER, 0, 0);
+        updateSystemBarsForPopup(activity);
+
+        UserDataManager.getInstance().getUserLiveData().observeForever(users -> {
+            if (adapter != null) {
+                adapter.submitList(users);
+            }
+        });
     }
 
     private int getResId(Context context, String name, String type) {
         return context.getResources().getIdentifier(name, type, context.getPackageName());
     }
 
-    public class UserViewHolder extends RecyclerView.ViewHolder {
-        LinearLayout anchorMonitorItemRoot;
-        ImageView avatar;
-        TextView userName, liveId, uid, uuid, encUid;
-        CheckBox strongRemind, voiceRemind, joinLive, avatarDownload;
+    private void saveOriginalSystemBarState(Activity activity) {
+        Window window = activity.getWindow();
+        originalStatusBarColor = window.getStatusBarColor();
+        originalNavigationBarColor = window.getNavigationBarColor();
+        originalSystemUiVisibility = window.getDecorView().getSystemUiVisibility();
+        originalLightStatusBars = (originalSystemUiVisibility & View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR) != 0;
+        originalLightNavBars = (originalSystemUiVisibility & View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR) != 0;
+    }
 
-        UserViewHolder(View itemView) {
-            super(itemView);
-            anchorMonitorItemRoot = itemView.findViewById(getResId(context, "anchor_monitor_item_root", "id"));
-            avatar = itemView.findViewById(getResId(context, "avatar", "id"));
-            userName = itemView.findViewById(getResId(context, "user_name", "id"));
-            uid = itemView.findViewById(getResId(context, "uid", "id"));
-            uuid = itemView.findViewById(getResId(context, "uuid", "id"));
-            encUid = itemView.findViewById(getResId(context, "enc_uid", "id"));
-            liveId = itemView.findViewById(getResId(context, "live_id", "id"));
-            strongRemind = itemView.findViewById(getResId(context, "strong_remind", "id"));
-            voiceRemind = itemView.findViewById(getResId(context, "voice_remind", "id"));
-            joinLive = itemView.findViewById(getResId(context, "join_live", "id"));
-            avatarDownload = itemView.findViewById(getResId(context, "avatar_download", "id"));
-        }
+    private void restoreOriginalSystemBarState(Activity activity) {
+        Window window = activity.getWindow();
+        window.setStatusBarColor(originalStatusBarColor);
+        window.setNavigationBarColor(originalNavigationBarColor);
 
-        @SuppressLint({"SetTextI18n", "UseCompatLoadingForDrawables", "DiscouragedApi"})
-        void bind(User user) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-            strongRemind.setOnCheckedChangeListener(null);
-            voiceRemind.setOnCheckedChangeListener(null);
-            joinLive.setOnCheckedChangeListener(null);
-            avatarDownload.setOnCheckedChangeListener(null);
+        int newVisibility = originalSystemUiVisibility;
+        newVisibility &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        newVisibility &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
 
-            int iconUserFace = getResId(context, "icon_user_face", "drawable");
-            Drawable checkBgDrawable;
-            if (user.getUid().equals(currentCheckingUid)) {
-                checkBgDrawable = new Gradient()
-                        .setColorLeft("#FFA500")
-                        .setColorRight("#FF8C00")
-                        .setRadius(14f)
-                        .build();
-            } else {
-                checkBgDrawable = AppContainer.getInstance().getModuleRes().getDrawable(iconUserFace, null);
+        if (originalLightStatusBars) newVisibility |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        if (originalLightNavBars) newVisibility |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+
+        window.getDecorView().setSystemUiVisibility(newVisibility);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController insetsController = window.getInsetsController();
+            if (insetsController != null) {
+                insetsController.setSystemBarsAppearance(
+                        originalLightStatusBars ? WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS : 0,
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+                insetsController.setSystemBarsAppearance(
+                        originalLightNavBars ? WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS : 0,
+                        WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
             }
-            anchorMonitorItemRoot.setBackground(checkBgDrawable);
-
-            Glide.with(context)
-                    .load(user.getAvatar())
-                    .placeholder(iconUserFace)
-                    .error(0)
-                    .into(avatar);
-
-            userName.setText(user.getName());
-            liveId.setText("直播ID " + user.getLive());
-            uid.setText("注册ID " + user.getUid());
-            uuid.setText("用户ID " + user.getUnion_uid());
-
-            if (user.getEnc_uid().isEmpty()) {
-                encUid.setVisibility(View.GONE);
-            } else {
-                encUid.setVisibility(View.VISIBLE);
-                encUid.setText("加密ID " + user.getEnc_uid());
-            }
-
-            strongRemind.setChecked(user.isStrongRemind());
-            voiceRemind.setChecked(user.isVoiceRemind());
-            joinLive.setChecked(user.isJoinLive());
-            avatarDownload.setChecked(user.isAvatarDownload());
-
-            strongRemind.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                user.setStrongRemind(isChecked);
-                dbManager.updateUserStrongRemind(user.getUid(), isChecked);
-            });
-
-            voiceRemind.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                user.setVoiceRemind(isChecked);
-                dbManager.updateUserVoiceRemind(user.getUid(), isChecked);
-            });
-
-            joinLive.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                user.setJoinLive(isChecked);
-                dbManager.updateUserJoinLive(user.getUid(), isChecked);
-            });
-
-            avatarDownload.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                user.setAvatarDownload(isChecked);
-                dbManager.updateUserAvatarDownload(user.getUid(), isChecked);
-            });
-
-            itemView.setOnLongClickListener(v -> {
-                DeleteConfirmationDialog.show(context, user.getName(), new DeleteConfirmationDialog.DeleteConfirmationListener() {
-                    @Override
-                    public void onConfirmDelete() {
-                        deleteListener.onUserDelete(user);
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        // 用户取消操作，不做任何处理
-                    }
-                });
-                return true;
-            });
         }
     }
 
-    static class UserDiffCallback extends DiffUtil.ItemCallback<User> {
-        @Override
-        public boolean areItemsTheSame(@NonNull User oldItem, @NonNull User newItem) {
-            return oldItem.getUid().equals(newItem.getUid());
+    private void updateSystemBarsForPopup(Activity activity) {
+        Window window = activity.getWindow();
+        int newColor = Color.parseColor("#FF1A1A1A");
+        window.setStatusBarColor(newColor);
+        window.setNavigationBarColor(newColor);
+
+        int newVisibility = window.getDecorView().getSystemUiVisibility();
+        newVisibility &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        newVisibility &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+
+        boolean isLightColor = isColorLight(newColor);
+        if (isLightColor) {
+            newVisibility |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            newVisibility |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
         }
 
-        @Override
-        public boolean areContentsTheSame(@NonNull User oldItem, @NonNull User newItem) {
-            return oldItem.getName().equals(newItem.getName())
-                    && oldItem.getLive().equals(newItem.getLive())
-                    && oldItem.isStrongRemind() == newItem.isStrongRemind()
-                    && oldItem.isVoiceRemind() == newItem.isVoiceRemind()
-                    && oldItem.isJoinLive() == newItem.isJoinLive()
-                    && oldItem.isAvatarDownload() == newItem.isAvatarDownload();
+        window.getDecorView().setSystemUiVisibility(newVisibility);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController insetsController = window.getInsetsController();
+            if (insetsController != null) {
+                insetsController.setSystemBarsAppearance(
+                        isLightColor ? WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS : 0,
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+                insetsController.setSystemBarsAppearance(
+                        isLightColor ? WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS : 0,
+                        WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
+            }
         }
     }
-}
+
+    private boolean isColorLight(int color) {
+        double darkness = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255;
+        return darkness < 0.5;
+    }
+
+    public void dismiss() {
+        if (popupWindow != null && popupWindow.isShowing()) {
+            restoreOriginalSystemBarState((Activity) popupWindow.getContentView().getContext());
+            popupWindow.dismiss();
+            popupWindow = null;
+        }
+    }
+
+    private void startChecking() {
+        if (adapter == null || adapter.getCurrentList().isEmpty()) {
+            Toast.makeText(popupWindow.getContentView().getContext(), "用户列表为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        isChecking = true;
+        currentCheckIndex = 0;
+        handler = new Handler(Looper.getMainLooper());
+
+        checkRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (!isChecking || adapter == null) return;
+
+                List<User> users = adapter.getCurrentList();
+                if (currentCheckIndex >= users.size()) {
+                    currentCheckIndex = 0;
+                }
+
+                User currentUser = users.get(currentCheckIndex);
+                adapter.setCurrentCheckingUid(currentUser.getUid());
+                checkUserHomepage(currentUser);
+                currentCheckIndex++;
+                handler.postDelayed(this, 5000);
+            }
+        };
+
+        handler.post(checkRunnable);
+    }
+
+    private void stopChecking() {
+        isChecking = false;
+        anchorStartButton.setText("开始定时检测");
+        if (handler != null && checkRunnable != null) {
+            handler.removeCallbacks(checkRunnable);
+        }
+    }
+
+    private void checkUserHomepage(User user) {
+        String uid = user.getUid();
+        Map<String, String> authMap = AuthManager.auHook
